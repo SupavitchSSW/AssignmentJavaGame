b1a57911a3ce3a0abd6c210882e4de2aa238188f
Sister smile <Sister smile>
2017-04-01 23:11:15 +0700

    edit set status

--- core/src/com/mygdx/game/Grizzly.java
+++ core/src/com/mygdx/game/Grizzly.java
@@ -5,47 +5,74 @@ package com.mygdx.game;
  */
 public class Grizzly extends Character {
 
-    int buffTime1 = 0;
+    int buffTime = 0;
 
     public Grizzly(int x, int y) {
         super(x, y);
     }
 
 
+    public void checkMana(){
+        switch (nextAction){
+            case 1: {
+                if (mana - 1 < 0) {
+                    nextAction = 7;
+                }
+                break;
+            }
+            case 4:{
+                if(mana - 2 < 0){
+                    nextAction = 7;
+                }
+                break;
+            }
+            case 5:{
+                if(mana - 4 < 0){
+                    nextAction = 7;
+                }
+            }
+        }
+    }
+
     public void setStatus(){
-        if(buffTime1 > 1){
-            defBuff = 100;
-            buffTime1--;
+        if(buffTime > 1){
+            atkBuff = 100 ;
+            buffTime--;
         }
-        if(nextAction == 4) { // skill1 + atk 100 3turn
+
+        if(nextAction == 4){ // skill1 + Def 100 3turn
             //isBuffActivate = true;
-            buffTime1 += 3;
+            buffTime += 3;
             atkBuff = 100;
+            mana -= 2;
         }
-        else if(nextAction == 5){ // skill2 600
-            atkBuff = 50;
-            atk = 300;
+        else if(nextAction == 5){ // skill2 300 Dmg + 50 posion 2 turn
+            //isBuff2Activate = true
+            atk = 600;
+            mana -= 4;
         }
         else if(nextAction == 1){ // normal attack 100 Dmg
-            atk = 100;
+            atk = 150;
+            mana -= 1;
         }
         else if(nextAction == 2){ // defedd 100 Def
             def = 100;
         }
-        else if(nextAction == 3) { // charge 2 mana
-            mana += 2;
+        else if(nextAction == 3){ // charge 2 mana
+            mana += 1;
+        }else{
+            nextAction = 0;
         }
 
         finalAtk = atk + atkBuff;
         finalDef = def + defBuff;
     }
 
-    public void reSetStatus(){
+    public void resetStatus(){
         atk = 0;
         def = 0;
         finalDef = 0;
         finalAtk = 0;
-
     }
 
 }
--- core/src/com/mygdx/game/Panda.java
+++ core/src/com/mygdx/game/Panda.java
@@ -9,46 +9,63 @@ public class Panda extends Character {
         super(x, y);
     }
 
+    public void checkMana(){
+        switch (nextAction){
+            case 1: {
+                if (mana - 1 < 0) {
+                    nextAction = 7;
+                }
+                break;
+            }
+            case 4:{
+                if(mana - 2 < 0){
+                    nextAction = 7;
+                }
+                break;
+            }
+            case 5:{
+                if(mana - 4 < 0){
+                    nextAction = 7;
+                }
+            }
+        }
+    }
+
     public void setStatus(){
-        if(buffTime1 > 1){
-            defBuff = 100;
-            buffTime1--;
-        }
-        if(buffTime2 > 1){
-            atkBuff = 50;
-            buffTime2--;
-        }
-        if(nextAction == 4) { // skill1 + Def 100 2turn
-            //isBuffActivate = true;
-            buffTime1 += 3;
-            defBuff = 100;
+
+        if(nextAction == 4){ // skill1 + Def 100 3turn
+
+            hp+=200;
+            mana -= 2;
         }
         else if(nextAction == 5){ // skill2 300 Dmg + 50 posion 2 turn
-            //isBuff2Activate = true;
-            buffTime2 += 2;
-            atkBuff = 50;
-            atk = 300;
+
+            atk = 400;
+            hp+=400;
+            mana -= 4;
         }
         else if(nextAction == 1){ // normal attack 100 Dmg
             atk = 100;
+            mana -= 1;
         }
         else if(nextAction == 2){ // defedd 100 Def
             def = 100;
         }
-        else if(nextAction == 3) { // charge 2 mana
-            mana += 2;
+        else if(nextAction == 3){ // charge 2 mana
+            mana += 1;
+        }else{
+            nextAction = 0;
         }
 
         finalAtk = atk + atkBuff;
         finalDef = def + defBuff;
     }
 
-    public void reSetStatus(){
+    public void resetStatus(){
         atk = 0;
         def = 0;
         finalDef = 0;
         finalAtk = 0;
-
     }
 
 }
