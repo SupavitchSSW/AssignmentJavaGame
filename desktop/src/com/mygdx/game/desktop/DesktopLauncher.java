3f7fc87350b0fcb706d499dbec28f8417f5acd2e
beamconan <beamconan@gmail.com>
2017-04-17 02:26:49 +0700

    Update DesktopLauncher.java

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -2,13 +2,13 @@ package com.mygdx.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.mygdx.game.CuttieBattle;
+import com.mygdx.game.BearsBattle;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.height = 800;
 		config.width = 1600;
-		new LwjglApplication(new CuttieBattle(), config);
+		new LwjglApplication(new BearsBattle(), config);
 	}
 }
